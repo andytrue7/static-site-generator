9290ad5226d0c763226a881e4762ae9d656e5646
Andrii Trubchanynov <a.trubchanynov@andersenlab.com>
2026-08-16 18:46:55 +0300

    add split node

--- src/textnode.py
+++ src/textnode.py
@@ -24,3 +24,17 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+    def split_nodes_delimiter(self, old_nodes: list["TextNode"], delimiter: str, text_type: TextType):
+        new_nodes = []
+        for node in old_nodes:
+            if node.text_type == TextType.TEXT:
+                parts = node.text.split(delimiter)
+                for i, part in enumerate(parts):
+                    if i % 2 == 0:
+                        new_nodes.append(TextNode(part, TextType.TEXT))
+                    else:
+                        new_nodes.append(TextNode(part, text_type))
+            else:
+                new_nodes.append(node)
+        return new_nodes
