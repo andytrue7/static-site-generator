091d27ecf5d49f1da2b5f0e9010b5c8fce1c585e
Andrii Trubchanynov <a.trubchanynov@andersenlab.com>
2026-08-17 16:53:11 +0300

    add markdown blocks

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,3 +23,32 @@ class HTMLNode:
         if self.props is None:
             return ""
         return " " + " ".join([f"{key}=\"{value}\"" for key, value in self.props.items()])
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str, children: list[HTMLNode], props: dict[str, str] | None = None) -> None:
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if self.children is None:
+            return ""
+        children_html = "".join([child.to_html() for child in self.children])
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str | None, value: str, props: dict[str, str] | None = None) -> None:
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.tag is None:
+            return self.value
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from src.htmlnode import LeafNode
+
 
 class TextType(Enum):
     TEXT = "text"
@@ -24,3 +26,22 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)    
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        if text_node.url is None:
+            raise ValueError("Link text node must have a URL")
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        if text_node.url is None:
+            raise ValueError("Image text node must have a URL")
+        return LeafNode("img", text_node.text, {"src": text_node.url})
+    raise ValueError(f"Unknown text type: {text_node.text_type}")
